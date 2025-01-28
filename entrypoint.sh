30eb53e50abde88bc56af8baae2cd26f7d9df66a
RisedSky <risedsky@gmail.com>
2025-01-28 14:37:21 +0100

    feat: Add .gitignore and update compose.yaml and entrypoint.sh for server download improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,29 +3,29 @@
 KOC_SERVER_FILE_PATH=/data/KnockoutCityServer
 
 if [[ -d "$KOC_SERVER_FILE_PATH" && "${KOC_FORCE_SERVER_DOWNLOAD:-false}" == false ]]; then
-  echo "Server files already downloaded. Skipping download..."
+    echo "Server files already downloaded. Skipping download..."
 else
-  if [[ -z "$KOC_SERVER_DOWNLOAD_URL" ]]; then
-    echo "No Server Download Url provided. Can't download..."
-    exit 1
-  fi
-  if [[  "${KOC_SERVER_DOWNLOAD_LATEST_VERSION}" == false ]]; then
-    echo 'Server is searching for the latest version...'
-    LATEST_LINK=$(echo -n 'https://www.knockoutcity.com/private-server-edition' | grep -oP "https://chonky-delivery-network\.akamaized\.net/KnockoutCity-Server(.*).zip")
-    if [[ -z "$LATEST_LINK" ]]; then
-      echo "Coudln't find the latest version of Knockout City Server Edition. Going to download the URL in the yaml file"
-    else
-      echo "Found the latest version !"
-      KOC_SERVER_DOWNLOAD_URL=$LATEST_LINK
+    if [[ -z "$KOC_SERVER_DOWNLOAD_URL" ]]; then
+        echo "No Server Download Url provided. Can't download..."
+        exit 1
     fi
-  fi
+    # if [[  "${KOC_SERVER_DOWNLOAD_LATEST_VERSION}" == false ]]; then
+    #   echo 'Server is searching for the latest version...'
+    #   LATEST_LINK=$(echo -n 'https://www.knockoutcity.com/private-server-edition' | grep -oP "https://chonky-delivery-network\.akamaized\.net/KnockoutCity-Server(.*).zip")
+    #   if [[ -z "$LATEST_LINK" ]]; then
+    #     echo "Coudln't find the latest version of Knockout City Server Edition. Going to download the URL in the yaml file"
+    #   else
+    #     echo "Found the latest version !"
+    #     KOC_SERVER_DOWNLOAD_URL=$LATEST_LINK
+    #   fi
+    # fi
 
-  mkdir -p /data
-  cd /data || exit 1
+    mkdir -p /data
+    cd /data || exit 1
 
-  wget "$KOC_SERVER_DOWNLOAD_URL" -O KnockoutCity-Server.zip
-  unzip KnockoutCity-Server.zip
-  rm KnockoutCity-Server.zip
+    wget "$KOC_SERVER_DOWNLOAD_URL" -O KnockoutCity-Server.zip
+    unzip KnockoutCity-Server.zip
+    rm KnockoutCity-Server.zip
 fi
 
 echo ""
@@ -42,11 +42,11 @@ echo ""
 
 cd $KOC_SERVER_FILE_PATH || exit 1
 wine64 KnockoutCityServer.exe \
-  -backend_port="${KOC_BACKEND_PORT:-23600}" \
-  -server_min_port="${KOC_SERVER_MIN_PORT:-23600}" \
-  -server_max_port="${KOC_SERVER_MAX_PORT:-23699}" \
-  -secret="${KOC_SECRET}" \
-  -backend_db="${KOC_BACKEND_DB}" \
-  -backend_redis_db_host="${KOC_BACKEND_REDIS_DB_HOST}" \
-  -backend_redis_db_port="${KOC_BACKEND_REDIS_DB_PORT:-6379}" \
-  -backend_tunable_user_connections_max_per_backend="${KOC_BACKEND_MAX_PLAYER_CONNECTIONS:-10}"
+    -backend_port="${KOC_BACKEND_PORT:-23600}" \
+    -server_min_port="${KOC_SERVER_MIN_PORT:-23600}" \
+    -server_max_port="${KOC_SERVER_MAX_PORT:-23699}" \
+    -secret="${KOC_SECRET}" \
+    -backend_db="${KOC_BACKEND_DB}" \
+    -backend_redis_db_host="${KOC_BACKEND_REDIS_DB_HOST}" \
+    -backend_redis_db_port="${KOC_BACKEND_REDIS_DB_PORT:-6379}" \
+    -backend_tunable_user_connections_max_per_backend="${KOC_BACKEND_MAX_PLAYER_CONNECTIONS:-10}"
